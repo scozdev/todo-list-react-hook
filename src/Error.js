9687cbcacfba071491b69f088d3f168a42bdde7c
Selçuk Özdemir <selcuk-ozdemir79@hotmail.com>
2021-07-30 09:45:18 +0300

    fix error

--- src/Error.js
+++ src/Error.js
@@ -1,21 +1,22 @@
-import React, { useEffect } from 'react';
-import styled from 'styled-components';
-import { useGlobalContext } from './context';
+import React, { useEffect } from "react";
+import styled from "styled-components";
+import { useGlobalContext } from "./context";
 
 const Error = () => {
   const { error, removeAlert, todos } = useGlobalContext();
 
   useEffect(() => {
-    setTimeout(() => {
+    const timeout = setTimeout(() => {
       removeAlert();
     }, 3000);
+    return () => clearTimeout(timeout);
   }, [removeAlert, todos]);
 
   return <Wrapper error={error.sh}>{error.msg}</Wrapper>;
 };
 
 const Wrapper = styled.div`
-  display: ${(props) => (props.error ? 'block' : 'none')};
+  display: ${(props) => (props.error ? "block" : "none")};
   color: red;
   font-size: 1.2rem;
   text-align: center;
